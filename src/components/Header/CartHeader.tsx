a109a3a837ad79c999fd0aa52b93ccd52bb0ff9e
faransn <faran.sanaei@gmail.com>
2023-03-03 17:18:58 +0330

    Create shopping cart page

--- src/components/Header/CartHeader.tsx
+++ src/components/Header/CartHeader.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useContext } from "react";
 import cartContext from "../../store/Cart/cart-context";
+import { Link } from "react-router-dom";
 
 function CartHeader() {
   const cartCtx = useContext(cartContext);
@@ -10,12 +11,14 @@ function CartHeader() {
   }, 0);
 
   return (
+    <Link to={"/cart"}>
       <div className="flex justify-end">
         <p className="grid content-center">Cart&nbsp;&nbsp;</p>
         <span className="text-white rounded-full bg-red-700 animate-pulse">
           &nbsp;&nbsp;{numberOfCartItems}&nbsp;&nbsp;
         </span>
       </div>
+    </Link>
   );
 }
 
